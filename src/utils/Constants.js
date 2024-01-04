79056b14c27277d825f5d1ffedb5fa0cb547185d
Xeeshanmohammad <95926440+Xeeshanmohammad@users.noreply.github.com>
2024-01-05 00:38:00 +0530

    Update Constants.js

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,3 @@
-export const Youtube_Video_Api =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyAb0k_tHtUJ82E3bES4CZbxd2XtCMcMx54";
 
-  export const Youtube_Suggestions_Search_Api =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
   export const Live_Chat = 30
